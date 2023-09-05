cb9c6998c52ce808758701bd405e3e227d7badae
ftr9 <ferlandodotel16@gmail.com>
2023-09-05 10:23:09 +0545

    Feat: Added one more action CHECK_AUTH to authReducer

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,16 +1,17 @@
-
-const authReducer = (initialState = { user: null, status: null }, action) => {
-
-    switch (action.type) {
-
-        case 'LOGGED_USER':
-            return { user: action.body, status: 'logged' }
-        case 'NOT_LOGGED_USER':
-            return { user: false, status: 'notlogged' }
-        default:
-            return initialState;
-    }
-
-}
+const authReducer = (
+  initialState = { user: null, status: null, isAuthenticating: false },
+  action
+) => {
+  switch (action.type) {
+    case 'LOGGED_USER':
+      return { user: action.body, status: 'logged', isAuthenticating: false };
+    case 'NOT_LOGGED_USER':
+      return { user: false, status: 'notlogged', isAuthenticating: false };
+    case 'CHECK_AUTH':
+      return { ...initialState, isAuthenticating: true };
+    default:
+      return initialState;
+  }
+};
 
 export default authReducer;
