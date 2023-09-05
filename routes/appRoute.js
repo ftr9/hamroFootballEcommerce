fa5de349ab96c28e89b6bd794cc4c98d9d14fb7f
ftr9 <ferlandodotel16@gmail.com>
2023-09-05 07:03:40 +0545

    Refactor: Separated payment routes

--- routes/appRoute.js
+++ routes/appRoute.js
@@ -1,14 +1,15 @@
-const router = require("express").Router();
-
+const router = require('express').Router();
 
 const appController = require('../controllers/appController.js');
-router.get("/list/:parameter", appController.getList);
-router.get("/userInfo", appController.getuserInfo);
-router.post("/order", appController.postOrder);
-router.get("/myorders", appController.getMyOrders);
-router.get("/adminorders", appController.getAdminOrders);
-router.post("/deleteorder/:id", appController.deleteOrders);
-router.post("/stripe", appController.StripePayment);
+router.get('/list/:parameter', appController.getList);
+router.get('/userInfo', appController.getuserInfo);
 
+router.get('/myorders', appController.getMyOrders);
+router.get('/adminorders', appController.getAdminOrders);
+router.post('/deleteorder/:id', appController.deleteOrders);
+
+/**Payment Part */
+router.post('/stripe', appController.StripePayment);
+router.post('/order', appController.postOrder);
 
 module.exports = router;
