434e6d6cbd75377600f88a0daade0a13277a8edb
ftr9 <ferlandodotel16@gmail.com>
2023-09-05 10:02:39 +0545

    Feat: Removed Passport authentication

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { auth } = require('express-openid-connect');
-const passport = require('passport');
-const cookieSession = require('cookie-session');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/keys.env' });
 
@@ -26,16 +24,6 @@ const config = {
 
 app.use(auth(config));
 
-require('./utils/passport');
-app.use(
-  cookieSession({
-    keys: [process.env.COOKIE_KEY],
-    maxAge: 1000 * 60 * 60,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
 //SAMPLE
 app.get('/', (req, res) => {
   if (req.oidc.isAuthenticated()) {
@@ -51,9 +39,6 @@ app.get('/user', (req, res) => {
 
 const appRoute = require('./routes/appRoute');
 app.use('/api/v1/hamrofootball', appRoute);
-
-const authRoute = require('./routes/authRoute');
-app.use('/auth/google/', authRoute);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join('client/build')));
