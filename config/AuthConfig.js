f649bdc062d7706ef954b31865b5d91661531485
ftr9 <ferlandodotel16@gmail.com>
2023-09-06 04:21:23 +0545

    Feat: Final build

---
+++ config/AuthConfig.js
@@ -0,0 +1,8 @@
+exports.config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3010',
+  clientID: 'LXOadcf7MSNTd34YkIicGQ3dtOLNMzVq',
+  issuerBaseURL: 'https://rahuldotel.jp.auth0.com',
+};
