85fa0a47f57f6187632ed0bb177d34f6963850e5
ftr9 <ferlandodotel16@gmail.com>
2023-09-05 07:04:44 +0545

    Refactor: Refactored to return state variable

--- client/src/redux/reducers/cartReducer.js
+++ client/src/redux/reducers/cartReducer.js
@@ -1,19 +1,15 @@
-const items = localStorage.getItem("addedToCart");
-const cartReducer = (state, action) => {
-
-    switch (action.type) {
-
-        case 'change':
-            const data = action.body
-            return data;
-        default:
-            if (items) {
-                return JSON.parse(localStorage.getItem("addedToCart"));
-            } else {
-                return {}
-            }
-    }
-
-}
+const items = localStorage.getItem('addedToCart');
+const cartReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'change':
+      const data = action.body;
+      return data;
+    default:
+      if (items) {
+        return JSON.parse(localStorage.getItem('addedToCart'));
+      }
+      return state;
+  }
+};
 
 export default cartReducer;
