64b55edba2c22b3c5fb7958a2951db57e04e3465
ftr9 <ferlandodotel16@gmail.com>
2023-09-05 16:15:52 +0545

    Feat: Added login and logout proxy

--- client/src/setupproxy.js
+++ client/src/setupproxy.js
@@ -1,51 +1,67 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = (app) => {
-    /*app.use(createProxyMiddleware("/api/v1/*", {
+module.exports = app => {
+  /*app.use(createProxyMiddleware("/api/v1/*", {
         target: 'http://localhost:3010',
         headers: {
             'Connection': 'keep-alive'
         }
     }))*/
 
-    app.use(
-        '/api/v1/hamrofootball/adminorders',
-        createProxyMiddleware({
-            target: 'http://localhost:3010',
-            changeOrigin: true,
-        })
-    );
+  app.use(
+    '/login',
+    createProxyMiddleware({
+      target: 'http://localhost:3010',
+      changeOrigin: true,
+    })
+  );
 
-    app.use(
-        '/api/v1/hamrofootball/myorders',
-        createProxyMiddleware({
-            target: 'http://localhost:3010',
-            changeOrigin: true,
-        })
-    );
+  app.use(
+    '/logout',
+    createProxyMiddleware({
+      target: 'http://localhost:3010',
+      changeOrigin: true,
+    })
+  );
 
-    app.use(
-        '/api/v1/hamrofootball/stripe',
-        createProxyMiddleware({
-            target: 'http://localhost:3010',
-            changeOrigin: true,
-        })
-    );
+  app.use(
+    '/api/v1/hamrofootball/adminorders',
+    createProxyMiddleware({
+      target: 'http://localhost:3010',
+      changeOrigin: true,
+    })
+  );
 
+  app.use(
+    '/api/v1/hamrofootball/myorders',
+    createProxyMiddleware({
+      target: 'http://localhost:3010',
+      changeOrigin: true,
+    })
+  );
 
+  app.use(
+    '/api/v1/hamrofootball/stripe',
+    createProxyMiddleware({
+      target: 'http://localhost:3010',
+      changeOrigin: true,
+    })
+  );
 
-    app.use(
-        '/api/v1/*',
-        createProxyMiddleware({
-            target: 'http://localhost:3010',
-            changeOrigin: true,
-        })
-    );
+  app.use(
+    '/api/v1/*',
+    createProxyMiddleware({
+      target: 'http://localhost:3010',
+      changeOrigin: true,
+    })
+  );
 
-    app.use(createProxyMiddleware("/auth/google/*", {
-        target: 'http://localhost:3010',
-        headers: {
-            'Connection': 'keep-alive'
-        }
-    }))
-}
+  app.use(
+    createProxyMiddleware('/auth/google/*', {
+      target: 'http://localhost:3010',
+      headers: {
+        Connection: 'keep-alive',
+      },
+    })
+  );
+};
