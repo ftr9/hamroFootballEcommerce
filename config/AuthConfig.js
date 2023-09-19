675780750286bc7585a2dc3c783285d9f96b79e7
ftr9 <ferlandodotel16@gmail.com>
2023-09-19 07:53:09 +0545

    FIX: gitignore error

--- config/AuthConfig.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3010,https://ecommerce6thsem.azurewebsites.net',
-  clientID: 'LXOadcf7MSNTd34YkIicGQ3dtOLNMzVq',
-  issuerBaseURL: 'https://rahuldotel.jp.auth0.com',
-};
---
+++ config/example.authConfig.js
@@ -0,0 +1,8 @@
+exports.config = {
+  authRequired: 'boolean',
+  auth0Logout: 'boolean',
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3010',
+  clientID: '<Your_secre_heret>',
+  issuerBaseURL: '<your_base_url_from_autho_dashboard>',
+};
